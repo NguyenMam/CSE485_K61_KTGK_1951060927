3bf04216b6a096db3813f8ceab0da559ef5327be
Nguyễn Thu Phương <90679641+NguyenMam@users.noreply.github.com>
2021-10-21 15:24:54 +0700

    Update error.php

--- src/error.php
+++ src/error.php
@@ -1,3 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hệ thống quản lý bệnh viện</title>
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+</head>
+
+<body>
 <div class=" text -center" >
     <h1><b>404 - NOT FOUND YOUR WEB</b></h1>
+     <a href="../src/index.php">You can comeback your web by click this content</a>
 </div>
+    </body>
+</html>
